3ba1d58026646917ddee4a960d24d176418c636a
Łukasz <kukis13@gmail.com>
2015-06-06 17:37:04 +0200

    Dodanie ikon pulpitu win95

--- EclipseProject/Domowa/WebContent/index.js
+++ EclipseProject/Domowa/WebContent/index.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $('#start-button').click(function(){
-        $(this).toggleClass("bar-button-down");
-    });
+$(document).ready(function() {
+	$('#start-button').click(function() {
+		$(this).toggleClass("bar-button-down");
+	});
+	var prvspam = $("#prv_main_link");
+	if (prvspam[0]) {
+		prvspam[0].parentNode.removeChild(element);
+	}
 });
