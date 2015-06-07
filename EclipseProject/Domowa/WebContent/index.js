66a4d84837b3059fa4924b0b37d95e3184902de4
Łukasz <kukis13@gmail.com>
2015-06-07 10:10:14 +0200

    Zaznaczanie ikonek odznacza inne.

--- EclipseProject/Domowa/WebContent/index.js
+++ EclipseProject/Domowa/WebContent/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-	$('#start-button').click(function() {
-		$(this).toggleClass("bar-button-down");
-	});
 	var prvspam = $("#prv_main_link");
 	if (prvspam[0]) {
 		prvspam[0].parentNode.removeChild(element);
 	}
+	
+	$('#start-button').click(function() {
+		$(this).toggleClass("bar-button-down");
+	});
+	
+	$('.icon').click(function(){
+		var icons = $('.icon');
+		for(i=0; i<icons.length; i++){
+			$(icons[i]).removeClass('icon-marked');
+		}
+		$(this).toggleClass("icon-marked");
+	})
 });
