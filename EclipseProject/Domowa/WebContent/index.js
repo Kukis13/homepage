be48475abe4ebce7e4a42e79d6343cc71838381d
Łukasz Jarocki <Lukasz.Jarocki@ATREM.PL>
2015-06-23 12:03:46 +0100

    Poprawa błędu z zaznaczaniem ikon

--- EclipseProject/Domowa/WebContent/index.js
+++ EclipseProject/Domowa/WebContent/index.js
@@ -1,80 +1,85 @@
 $(document).ready(function() {
-	removeAd();
-	registerClickListeners();
+    removeAd();
+    registerClickListeners();
 });
 
 function removeAd() {
-	var prvspam = $("#prv_main_link");
-	if (prvspam[0]) {
-		prvspam[0].parentNode.removeChild(element);
-	}
+    var prvspam = $("#prv_main_link");
+    if (prvspam[0]) {
+        prvspam[0].parentNode.removeChild(element);
+    }
 }
 
 function registerClickListeners() {
-	$('body').click(function() {
-		resetIcons();
-	});
-	$('#start-button').click(function() {
-		$(this).toggleClass("bar-button-down");
-	});
+    $('body').click(function() {
+        resetIcons();
+    });
+    $('#start-button').click(function() {
+        $(this).toggleClass("bar-button-down");
+    });
 
-	$('.icon').click(function(e) {
-		resetIcons();
-		$(this).toggleClass("icon-marked");
-		if ($(e.target).is('span') && !$(this).hasClass('icon-marked')) {
-			$(e.target).attr('contentEditable', true);
-			$(e.target).focus();
-			$(this).addClass('icon-text-editable');
-		}
-		e.stopPropagation();
-	});
+    $('.icon').click(function(e) {
+        var icons = $('.icon');
+        for (i = 0; i < icons.length; i++) {
+            if (icons[i] != this) {
+                $(icons[i]).removeClass('icon-marked');
+                $(icons[i]).removeClass('icon-text-editable');
+                $(icons[i]).find('span').attr('contentEditable', false);
+            }
+        }
+        $(this).toggleClass("icon-marked");
+        if ($(e.target).is('span') && !$(this).hasClass('icon-marked')) {
+            $(e.target).attr('contentEditable', true);
+            $(e.target).focus();
+            $(this).addClass('icon-text-editable');
+        }
+        e.stopPropagation();
+    });
 
-	$('.icon').dblclick(function(e) {
-		var target = e.currentTarget;
-		if ($(target).hasClass('icon')) {
-			openApplication(target.id);
-			return;
-		}
-		var parents = $(e.currentTarget).parents();
-		for (i = 0; i < parents.length; i++) {
-			if ($(parents[i]).hasClass('icon')) {
-				openApplication(parents[i].id);
-			}
-		}
-		openApplication(e);
-	});
+    $('.icon').dblclick(function(e) {
+        var target = e.currentTarget;
+        if ($(target).hasClass('icon')) {
+            openApplication(target.id);
+            return;
+        }
+        var parents = $(e.currentTarget).parents();
+        for (i = 0; i < parents.length; i++) {
+            if ($(parents[i]).hasClass('icon')) {
+                openApplication(parents[i].id);
+            }
+        }
+        openApplication(e);
+    });
 
-	$("#desktop").mousedown(
-			function(e) {
+    $("#desktop").mousedown(function(e) {
 
-				var underElement = document.elementFromPoint(e.pageX, e.pageY);
-				if ($(underElement).hasClass('icon')
-						|| $(underElement).parent().hasClass('icon')) {
-					return;
-				} else {
-					$('body').click();
-				}
-				$(".ghost-select").addClass("ghost-active");
-				$(".ghost-select").css({
-					'left' : e.pageX,
-					'top' : e.pageY
-				});
+        var underElement = document.elementFromPoint(e.pageX, e.pageY);
+        if ($(underElement).hasClass('icon') || $(underElement).parent().hasClass('icon')) {
+            return;
+        } else {
+            $('body').click();
+        }
+        $(".ghost-select").addClass("ghost-active");
+        $(".ghost-select").css({
+            'left': e.pageX,
+            'top': e.pageY
+        });
 
-				initialW = e.pageX;
-				initialH = e.pageY;
+        initialW = e.pageX;
+        initialH = e.pageY;
 
-				$(document).bind("mousemove", openSelector);
-				$(document).bind("mouseup", afterMouseUp);
-			});
+        $(document).bind("mousemove", openSelector);
+        $(document).bind("mouseup", afterMouseUp);
+    });
 }
 
 function openApplication(applicationName) {
-	resetIcons();
-	switch (applicationName) {
-	case 'ico-cv':
-		showCV();
-		break;
-	default:
-		showBSOD();
-	}
+    resetIcons();
+    switch (applicationName) {
+    case 'ico-cv':
+        showCV();
+        break;
+    default:
+        showBSOD();
+    }
 }
